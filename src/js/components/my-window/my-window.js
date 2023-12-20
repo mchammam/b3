3016f0a5cbbac1734ceab8bd31717ca708eece8d
Mohammed Chammam <mc223gr@student.lnu.se>
2023-12-20 20:35:02 +0100

    Add logic to move the window.

--- src/js/components/my-window/my-window.js
+++ src/js/components/my-window/my-window.js
@@ -15,10 +15,12 @@ template.innerHTML = `
     left: 0;
     border: 1px solid;
   }
+  #title {
+    cursor: default;
+    user-select: none;
+  }
 </style>
-<div id="title">
-
-</div>
+<div id="title"></div>
 <slot>
 </slot>
 `
@@ -29,6 +31,20 @@ customElements.define(
    * Represents a quiz-application element.
    */
   class extends HTMLElement {
+    /**
+     * Bound event handler.
+     *
+     * @type {Function}
+     */
+    #boundHandleMouseMove
+
+    /**
+     * Bound event handler.
+     *
+     * @type {Function}
+     */
+    #boundHandleMouseDown
+
     /**
      * The user nickname.
      *
@@ -55,7 +71,7 @@ customElements.define(
      * @returns {string[]} A string array of attributes to monitor.
      */
     static get observedAttributes () {
-      return ['title']
+      return ['title', 'top', 'left']
     }
 
     /**
@@ -69,20 +85,58 @@ customElements.define(
       if (name === 'title' && oldValue !== newValue) {
         this.shadowRoot.querySelector('#title').textContent = newValue
       }
+
+      if (name === 'top' && oldValue !== newValue) {
+        this.shadowRoot.host.style.top = newValue + 'px'
+      }
+
+      if (name === 'left' && oldValue !== newValue) {
+        this.shadowRoot.host.style.left = newValue + 'px'
+      }
     }
 
     /**
      * Called after the element is inserted into the DOM.
      */
     connectedCallback () {
+      /**
+       * Method to handle mouse down on window title div (and move the window).
+       *
+       * @param {MouseEvent} event - The mouse down event.
+       */
+      this.shadowRoot.querySelector('#title').addEventListener('mousedown', this.#boundHandleMouseDown = event => {
+        if (event.button !== 0) {
+          return
+        }
 
+        const windowTop = this.getAttribute('top') ?? 0
+        const mouseToWindowOffsetY = event.pageY - windowTop
+
+        const windowLeft = this.getAttribute('left') ?? 0
+        const mouseToWindowOffsetX = event.pageX - windowLeft
+
+        // TODO: is this the best way?
+        /**
+         * Method to handle mouse move.
+         *
+         * @param {MouseEvent} event - The mouse move event.
+         */
+        window.addEventListener('mousemove', this.#boundHandleMouseMove = event => {
+          this.setAttribute('top', event.pageY - mouseToWindowOffsetY)
+          this.setAttribute('left', event.pageX - mouseToWindowOffsetX)
+        })
+
+        window.addEventListener('mouseup', event => {
+          window.removeEventListener('mousemove', this.#boundHandleMouseMove)
+        }, { once: true })
+      })
     }
 
     /**
      * Called after the element has been removed from the DOM.
      */
     disconnectedCallback () {
-
+      this.shadowRoot.querySelector('#title').removeEventListener('mousedown', this.#boundHandleMouseDown)
     }
   }
 )
