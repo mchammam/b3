5a6f4608fb887cc7fb93b8caa002cdbaa86db414
Mohammed Chammam <mc223gr@student.lnu.se>
2024-01-19 10:17:51 +0100

    Add Service Worker and offline support

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,10 @@
  * @author Mohammed Chammam <mc223gr@student.lnu.se>
  * @version 1.0.0
  */
+
+try {
+  const registration = await navigator.serviceWorker.register('./sw.js')
+  console.log('ServiceWorker registration successful with scope: ', registration.scope)
+} catch (error) {
+  console.log('ServiceWorker registration failed: ', error)
+}
---
+++ src/sw.js
@@ -0,0 +1,44 @@
+const version = '1.0.0'
+
+self.addEventListener('install', async (event) => {
+  console.log(`ServiceWorker: Version ${version} installed`)
+})
+
+self.addEventListener('activate', async (event) => {
+  console.log(`ServiceWorker: Version ${version} activated`)
+
+  const cacheKeys = await caches.keys()
+
+  cacheKeys.forEach(key => {
+    if (key !== version) {
+      console.info('ServiceWorker: Clearing cache for version', key)
+      caches.delete(key)
+    }
+  })
+})
+
+self.addEventListener('fetch', (event) => {
+  console.log('ServiceWorker: Fetch')
+
+  event.respondWith(cachedfetch(event.request))
+})
+
+/**
+ * Method used to respond with cached data when fetch fails.
+ *
+ * @param {Request} request - The request object.
+ * @returns {Response | Promise<Response | undefined>} The response object.
+ */
+async function cachedfetch (request) {
+  try {
+    const res = await fetch(request)
+
+    const cache = await caches.open(version)
+    cache.put(request, res.clone())
+
+    return res
+  } catch {
+    console.info('ServiceWorker: Serving cached result')
+    return caches.match(request)
+  }
+}
