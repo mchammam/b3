20dcace66848c22cdc90b074b81a22db3751898d
Mohammed Chammam <mc223gr@student.lnu.se>
2024-01-22 22:53:44 +0100

    Add abort controller

--- src/js/components/my-memory-game-start-form/my-memory-game-start-form.js
+++ src/js/components/my-memory-game-start-form/my-memory-game-start-form.js
@@ -9,67 +9,68 @@
 const template = document.createElement('template')
 template.innerHTML = `
 <style>
-form{
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-  padding: 2rem 1rem;
-}
-.input-group {
-  position: relative;
-}
-#board-size-btns {
-  display: flex;
-  flex-direction: column;
-  gap: 0.75rem;
-}
-label {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background: #374151;
-  font-size: 1.2rem;
-  color: rgb(117, 117, 117);
-  cursor: text;
-  transition: all 200ms;
-}
-input:focus + label, input:valid + label{
-  top: 0;
-  left: 1.1rem;
-  transform: translateY(-50%);
-  padding-inline: 0.2rem;
-  font-size: 0.85rem;
-  color: unset;
-  letter-spacing: 0.06rem;
-  font-weight: bold;
-}
-input {
-  padding: 0.3rem;
-  background: #374151;
-  border-radius: 1rem;
-  border: 2px solid #E5E7EB;
-  font-size: 1.2rem;
-  text-align: center;
-  color: #E5E7EB;
-}
-button {
-  padding: 0.75rem 0.3rem;
-  background: #1F2937;
-  border-radius: 1rem;
-  border: 2px solid #1F2937;
-  font-size: 0.8rem;
-  font-weight: bold;
-  letter-spacing: 0.12rem;
-  color: #E5E7EB;
-  cursor: pointer;
-  transition: all 150ms;
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    padding: 2rem 1rem;
   }
-button:hover, button:focus {
-  background: #111827;
-  box-shadow: 0 3px 10px rgba(22, 25, 32, 0.25);
-}
+  .input-group {
+    position: relative;
+  }
+  #board-size-btns {
+    display: flex;
+    flex-direction: column;
+    gap: 0.75rem;
+  }
+  label {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background: #374151;
+    font-size: 1.2rem;
+    color: rgb(117, 117, 117);
+    cursor: text;
+    transition: all 200ms;
+  }
+  input:focus + label, input:valid + label {
+    top: 0;
+    left: 1.1rem;
+    transform: translateY(-50%);
+    padding-inline: 0.2rem;
+    font-size: 0.85rem;
+    color: unset;
+    letter-spacing: 0.06rem;
+    font-weight: bold;
+  }
+  input {
+    padding: 0.3rem;
+    background: #374151;
+    border-radius: 1rem;
+    border: 2px solid #E5E7EB;
+    font-size: 1.2rem;
+    text-align: center;
+    color: #E5E7EB;
+  }
+  button {
+    padding: 0.75rem 0.3rem;
+    background: #1F2937;
+    border-radius: 1rem;
+    border: 2px solid #1F2937;
+    font-size: 0.8rem;
+    font-weight: bold;
+    letter-spacing: 0.12rem;
+    color: #E5E7EB;
+    cursor: pointer;
+    transition: all 150ms;
+  }
+  button:hover, button:focus {
+    background: #111827;
+    box-shadow: 0 3px 10px rgba(22, 25, 32, 0.25);
+  }
 </style>
+
 <form>
   <div class="input-group">
     <input id="nickname" type="text" required/>
@@ -90,11 +91,11 @@ customElements.define(
    */
   class extends HTMLElement {
     /**
-     * Bound event handler.
+     * Used to remove the event listeners on component disconnect.
      *
-     * @type {Function}
+     * @type {AbortController}
      */
-    #boundHandleSubmit
+    #abortController = new AbortController()
 
     /**
      * Creates an instance of the current type.
@@ -141,37 +142,31 @@ customElements.define(
        *
        * @param {SubmitEvent} event - the submit event.
        */
-      this.shadowRoot.querySelector('form').addEventListener(
-        'submit',
-        (this.#boundHandleSubmit = (event) => {
+      this.shadowRoot.querySelector('form').addEventListener('submit', (event) => {
+        event.preventDefault()
+
+        this.#handleSubmit(event.submitter.getAttribute('data-board-size'))
+      }, { signal: this.#abortController.signal })
+
+      this.shadowRoot.querySelector('input').addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
           event.preventDefault()
-
-          this.#handleSubmit(event.submitter.getAttribute('data-board-size'))
-        })
-      )
-
-      this.shadowRoot.querySelector('input').addEventListener(
-        'keydown', event => {
-          if (event.key === 'Enter') {
-            event.preventDefault()
-          }
         }
-      )
+      }, { signal: this.#abortController.signal })
     }
 
     /**
      * Called after the element has been removed from the DOM.
      */
     disconnectedCallback () {
-      this.shadowRoot
-        .querySelector('form')
-        .removeEventListener('submit', this.#boundHandleSubmit)
+      // Remove the event listeners.
+      this.#abortController.abort()
     }
 
     /**
      * Handles the form submit event.
      *
-     * @param boardSize
+     * @param {'small'|'medium'|'large'} boardSize - The board size.
      */
     #handleSubmit (boardSize) {
       const nickname = this.shadowRoot.querySelector('#nickname').value
