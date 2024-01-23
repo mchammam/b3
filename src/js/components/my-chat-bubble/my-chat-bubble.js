cd73ee9f695f96ed99c5b0026751ad5b0cc9a176
Mohammed Chammam <mc223gr@student.lnu.se>
2024-01-23 19:22:00 +0100

    Fix styles

--- src/js/components/my-chat-bubble/my-chat-bubble.js
+++ src/js/components/my-chat-bubble/my-chat-bubble.js
@@ -5,6 +5,8 @@
  * @version 1.0.0
  */
 
+import '../my-spinner'
+
 // Define template.
 const template = document.createElement('template')
 template.innerHTML = `
@@ -15,13 +17,14 @@ template.innerHTML = `
     padding: 0.2rem 0.5rem;
     border-radius: 0 10px 10px 10px;
     background-color: #111827;
+    word-break: break-word;
   }
   :host([self]) {
     margin-left: auto;
     margin-right: 0;
     border-radius: 10px 0 10px 10px;
   }
-  #username {
+  .username-bar {
     position: absolute;
     left: 0;
     bottom: 0;
@@ -29,17 +32,35 @@ template.innerHTML = `
     font-size: 0.6rem;
     padding-inline: 0.2rem;
   }
-  :host([self]) #username {
+  :host([self]) .username-bar {
+    display: none;
     right: 0;
     left: auto;
   }
   slot {
     white-space: pre-line;
   }
+  my-spinner {
+    display: none;
+  }
+  my-spinner::part(ring) {
+    height: 9px;
+    width: 9px;
+  }
+  my-spinner::part(ring-div) {
+    width: 7px;
+    height: 7px;
+    margin: 3px;
+    border: 2px solid #fff;
+    border-color: #fff transparent transparent transparent;
+  }
 </style>
 
-<span id="username"></span>
 <slot></slot>
+<div class="username-bar">
+  <span id="username"></span>
+  <my-spinner></my-spinner>
+</div>
 `
 customElements.define(
   'my-chat-bubble',
