311c8a29813ee6fa76b66b001a3a49229aa1bf78
Mohammed Chammam <mc223gr@student.lnu.se>
2024-01-23 22:17:02 +0100

    Remove comments

--- src/sw.js
+++ src/sw.js
@@ -1,24 +1,24 @@
 const version = '1.0.0'
 
 self.addEventListener('install', async (event) => {
-  console.log(`ServiceWorker: Version ${version} installed`)
+  // console.log(`ServiceWorker: Version ${version} installed`)
 })
 
 self.addEventListener('activate', async (event) => {
-  console.log(`ServiceWorker: Version ${version} activated`)
+  // console.log(`ServiceWorker: Version ${version} activated`)
 
   const cacheKeys = await caches.keys()
 
   cacheKeys.forEach(key => {
     if (key !== version) {
-      console.info('ServiceWorker: Clearing cache for version', key)
+      // console.info('ServiceWorker: Clearing cache for version', key)
       caches.delete(key)
     }
   })
 })
 
 self.addEventListener('fetch', (event) => {
-  console.log('ServiceWorker: Fetch')
+  // console.info('ServiceWorker: Fetch')
 
   event.respondWith(cachedfetch(event.request))
 })
@@ -39,12 +39,11 @@ async function cachedfetch (request) {
     }
 
     const cache = await caches.open(version)
-    // console.log(request)
     cache.put(request, res.clone())
 
     return res
   } catch {
-    console.info('ServiceWorker: Serving cached result')
+    // console.info('ServiceWorker: Serving cached result')
     return caches.match(request)
   }
 }
